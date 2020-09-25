5d85c0ee6f3083339396893f702169127bd10925
Timothy Prestby <tjprestby@gmail.com>
2020-09-25 11:38:42 -0500

    Added highlighting

--- js/main.js
+++ js/main.js
@@ -21,8 +21,8 @@ function setMap(){
 
     //Create Albers equal area conic projection centered on China
     var projection = d3.geoAzimuthalEquidistant()
-        .center([-10.5, 43.061])
-        .scale(500000)
+        .center([-10.5, 43.062])
+        .scale(600000)
         .translate([width / 2, height / 2])
         .rotate([79, 0.0, 0.1]);
 
@@ -54,6 +54,7 @@ function setMap(){
 
         //draw routes
         drawFlows("3/30/2020",csvData,map, projection)
+
     };
 };
 
@@ -104,13 +105,9 @@ function createLabels(proj, uw, map){
 
 // Draw a set of routes 
 function drawFlows(date, data, map, proj) {
-    console.log(data)
-
-
 
     //create group
     var g1 = map.append("g");
-
 
     var maxVolume = -12;
     data.forEach(function(routes){
@@ -121,39 +118,19 @@ function drawFlows(date, data, map, proj) {
 
     var line = d3.line().curve(d3.curveBasis);
 
-    data.forEach( function(d,i) {
+    data.forEach( function(d) {
         var routePath = g1.append("path")	
             .attr("d", line ([ proj(centDic[d.geoid_o]),proj(centDic[d.geoid_d])]))
-            .attr("class", d.geoid_o.replace(" ", "-") + " " + "route")
+            .attr("class", "t"+ d.geoid_o + "_" + d.geoid_d + " route")
             .attr("stroke-opacity", Math.sqrt(d[date] / maxVolume) )
-            .attr("stroke-width", 1 );
-
-    var totalLength =  routePath.node().getTotalLength() + 10;
-        routePath
-            .attr("stroke-dasharray", totalLength + " " + totalLength)
-            .attr("stroke-dashoffset", totalLength)
-            .transition()
-            .duration(2000)
-            //.on("start", drawPorts(d) )
-            .attr("stroke-dashoffset", 0);
-    });
+            .attr("stroke-width", 2 )
+            //Create event listeners for highlighting and dehighlighting using mouse over
+            .on("mouseover", function () { highlight(d)})
+            .on("mouseout", function () { dehighlight(d)})
+            .on("mousemove", moveLabel);
+            
 
 
-
-
-
-    /*
-    d3.csv(date + ".csv", function (error, routes) {
-        var maxVolume = d3.max(routes, function(d) { return d.vol; });
-                    
-        var line = d3.line().curve(d3.curveBasis);
-
-        routes.forEach( function(d,i) {
-             var routePath = g1.append("path")	
-                .attr("d", line ([ projection(ports[d.dept].loc),projection(intermediatePoints[d.via]),projection(ports[d.arr].loc)]) )
-                .attr("class", d.arr.replace(" ", "-") + " " + "route")
-                .attr("stroke-opacity", Math.sqrt(d.vol / maxVolume) )
-                .attr("stroke-width", 1 );
 
         var totalLength =  routePath.node().getTotalLength() + 10;
             routePath
@@ -161,13 +138,94 @@ function drawFlows(date, data, map, proj) {
                 .attr("stroke-dashoffset", totalLength)
                 .transition()
                 .duration(2000)
-                .on("start", drawPorts(d) )
                 .attr("stroke-dashoffset", 0);
-        });
+
+        //Set default style for once flow is dehighlighted 
+        var desc = routePath.append("desc")
+            .text('{"stroke": "#333"}');
     });
-    */
+
+
+
 }	
 
+
+
+
+//Function: highlight enumeration units and bars//
+function highlight(props) {
+    console.log(props)
+    //Change the opacity of the highlighted item by selecting the class
+    var selected = d3.selectAll("." + "t" + props.geoid_o + "_" + props.geoid_d)
+        .style("stroke", "red");
+    //Call setlabel to create dynamic label
+    setLabel(props);
+};
+
+//Function: dehighlight regions//
+function dehighlight(props) {
+    var selected = d3.selectAll("." + "t" + props.geoid_o + "_" + props.geoid_d)
+        .style("stroke", function () {
+            //Get the unique opacity element for current DOM element within the desc element
+            return getStyle(this, "stroke")
+        });
+
+    //Create function that gets the description text of an element
+    function getStyle(element, styleName) {
+        //Select current DOM element
+        var styleText = d3.select(element)
+            .select("desc")
+            //Return text content in desc
+            .text();
+        //Create JSON string
+        var styleObject = JSON.parse(styleText);
+        return styleObject[styleName];
+    };
+    d3.select(".infolabel")
+        .remove();
+} 
+
+ //Function: create dynamic labels//
+ function setLabel(props) {
+	//Create label content as HTML string
+	var labelAttribute = '<h1>' + 'Travel volume from ' + props.geoid_o + ' tract to ' + props.geoid_d + ' tract: <i>' + props['3/30/2020'] + '</i></h1><br>';
+	//Create detailed label in html page 
+	var infolabel = d3.select('div#map')
+			.append('div')
+			//Define class, ID, and add it to HTML
+			.attr('class', 'infolabel')
+			.attr('id', 'label_' + props.geoid_o + "_" + props.geoid_d)
+            .html(labelAttribute);
+    
+};
+
+//Function: move label where mouse moves//
+function moveLabel() {
+    console.log('hi')
+    //Determine width of label
+    var labelWidth = d3.select('.infolabel')
+        //Use node() to get the first element in this selection
+        .node()
+        //Return an object containing the sie of the label
+        .getBoundingClientRect()
+        //Examine width to determine how much to shift the mouse over
+        .width;
+
+    //Use coordinates of mousemove event to set label coordinates with offsets from wherever event is
+    var x1 = d3.event.clientX + 10,
+        y1 = d3.event.clientY - 75,
+        x2 = d3.event.clientX - labelWidth - 10,
+        //Used to switch vertical sides
+        y2 = d3.event.clientY + 25;
+    //Test for overflow horizontally (If the event x coordinate is greater than the width of the window and label buffer)
+    var x = d3.event.clientX > window.innerWidth - labelWidth - 20 ? x2 : x1;
+    //Test for overflow vertically (Is the Y coordinate less than the distance between mouse and upper-left label)
+    var y = d3.event.clientY < 75 ? y2 : y1;
+    //Select the infolabel currently mousing over
+    d3.select(".infolabel")
+        .style("left", x + "px")
+        .style("top", y + "px");
+};
 
 
 //begin script when window loads
